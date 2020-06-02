06fab89604219a89057de49439af78c22442b9f6
fitzpaky <kfitzpatrick44@gmail.com>
2020-06-01 19:53:00 -0700

    deleting nodemodules and lock json

--- server.js
+++ server.js
@@ -25,11 +25,8 @@ app.use(express.static("public"));
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-mongoose.connect(MONGODB_URI);
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines");
 
-var db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 // Handlebars
 app.engine(
     "handlebars",
